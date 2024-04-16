7a3ad536afe2b70142006833849991ddcf67c44e
Djamel-yod <isma1yoda@gmail.com>
2024-04-16 12:11:30 +0200

    Fichier Streamlit

--- streamlit.py
+++ streamlit.py
@@ -12,7 +12,7 @@ best_model = load('bestModel.joblib')
 # Titre de l'application
 st.title('Prédiction des émissions de CO2 en France')
 st.subheader('Auteur: Ismael YODA')
-st.write("Cette application est destinée à éffectuer la prévision moyenne annuelle des émissions de CO2 des véhicules commercialisés en France. Elle prend en entrée les caractéristiques techniques du véhicule et sa consommation en carburant et elle renvoie en sortie la prédiction de sa moyenne d'émission annuelle de CO2.")
+st.write("Cette application est destinée à éffectuer la prédiction en g/km,les émissions de CO2 des véhicules commercialisés en France. Elle prend en entrée les caractéristiques techniques du véhicule et sa consommation en carburant et elle renvoie en sortie la prédiction de son émission de CO2 en g/km.")
 
 
 # Fonction de prédiction
@@ -39,4 +39,4 @@ masse_en_ordre_de_marche_maximale = st.number_input('Masse en ordre de marche ma
 # Bouton pour effectuer la prédiction
 if st.button('Prédire'):
     prediction = predict_CO2(puissance_administrative, consommation_extra_urbaine_de_carburant, masse_en_ordre_de_marche_minimale, masse_en_ordre_de_marche_maximale)
-    st.success(f'Prédiction de CO2: {prediction[0]}')
+    st.success(f'Prédiction de CO2 en g/km: {prediction[0]}')
